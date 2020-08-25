751ae4d4d77118734833366b47a90c5c773a1ce2
TonyXdddd <Winterkai@ukr.net>
2020-08-25 19:10:56 +0800

    Features section basicly done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,11 +3,22 @@
 document.getElementById("general-h").addEventListener("mouseover", function () {
     document.getElementById("bg-changed-wrapper").classList.add("section-home_changed-bg");
     document.getElementById("header-wrapper").classList.add("section-home__header_black-separator");
-    document.getElementById("section-features").classList.add("app__section-features_changed-bg")
+    document.getElementById("section-features").classList.add("app__section-features_changed-bg");
+
+    for (let i = 0; i < 6; i++) {
+        document.getElementsByClassName("features__item-text-wrapper")[i].classList.add("features__item-text-wrapper_black-bg");
+
+    }
 });
 
 document.getElementById("general-h").addEventListener("mouseout", function () {
     document.getElementById("bg-changed-wrapper").classList.remove("section-home_changed-bg");
     document.getElementById("header-wrapper").classList.remove("section-home__header_black-separator");
-    document.getElementById("section-features").classList.remove("app__section-features_changed-bg")
+    document.getElementById("section-features").classList.remove("app__section-features_changed-bg");
+
+    for (let i = 0; i < 6; i++) {
+        document.getElementsByClassName("features__item-text-wrapper")[i].classList.remove("features__item-text-wrapper_black-bg");
+
+    }
 });
+
